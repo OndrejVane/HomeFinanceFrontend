cb8d67f044b34c6fa19224d39492980f9c1f6f40
ondrejvane <onry.vane@gmail.com>
2025-12-09 13:26:34 +0100

    Oprava buildu

--- scripts/set-version.js
+++ scripts/set-version.js
@@ -2,4 +2,4 @@ const { writeFileSync } = require('fs');
 const { version } = require('../package.json');
 
 const content = `export const version = '${version}';`;
-writeFileSync('./src/environments/version.ts', content);
+writeFileSync('./src/version/version.ts', content);
